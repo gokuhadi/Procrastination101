8aad8734b2c9fd24eb38a57caa4f32721904da4a
Salman Hadi <43488691+gokuhadi@users.noreply.github.com>
2023-08-19 08:40:37 +0100

    Update Ticket_Rides.py

--- Ticket_Rides.py
+++ Ticket_Rides.py
@@ -1,15 +1,26 @@
 print ("Welcome to  the rollercoaster!!\n")
 
 height=int(input("What is your Height? "))
+bill=0
 
 if height >= 120:
     print ("You can ride! Do not puke.")
     age=int(input("Enter your age- "))
     if age>18:
+        bill=12
         print("Pay 12 Dollars")
-    elif age<12 :
+    elif age<12:
+        bill=5
         print("Pay 5 Dollars")
     else:
+        bill=7
         print("Pay 7 dollars")
+    wants_photo = input("Do you want a photo? Y or N ")
+    if wants_photo == "y":
+        bill+=3
+        print(f"Total is {bill}")
+    else:
+        print(f"Total bill without the pic is {bill}")
 else :
     print ("No rides for you! Sorry short kings..")
+  
